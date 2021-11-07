6051d2dfec8acb23218529b0328fb0360668de9e
vgrugor <vgrugor@gmail.com>
2021-11-07 18:33:26 +0200

    Create blog

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::view('/', 'home');
+
+Route::get('post/create', \App\Http\Livewire\PostCreate::class);
+
+Route::get('post/{slug}', \App\Http\Livewire\Post::class);
